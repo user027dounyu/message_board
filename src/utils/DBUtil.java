9552b60e91c7ab49f9f21a876e7b557904711542
hiroki.kitazawa <2023dounyukenshu.027@gmail.com>
2023-05-24 14:55:33 +0900

    getAllMessagesを追加

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -1,11 +1,11 @@
-package util;
+package utils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final String PERSISTENCE_UNIT_NAME="massage_board";
+    private static final String PERSISTENCE_UNIT_NAME="message_board";
     private static EntityManagerFactory emf;
 
     public static EntityManager createEntityManager() {
